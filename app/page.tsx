595b15248dd622a5312f73ee886b4bd89e2bfd48
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2025-12-23 08:36:38 +0530

    create authentication using github OAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
+import LogOut from "@/modules/auth/components/log-out";
+import { requireAuth } from "@/modules/auth/utils/auth-utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await requireAuth();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click Me</Button>
+      <LogOut>
+        <Button>Log out</Button>
+      </LogOut>
     </div>
   );
 }
